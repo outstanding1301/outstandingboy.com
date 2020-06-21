50116342e88028f0b7c194afcf28707d94f72836
outstandingboy <outstandingboy@kakao.com>
2020-06-21 18:51:43 +0900

    Updates

---
+++ precache-manifest.858a1d24bfecef35591d61fd8529cae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b0e95622a38b5025d598ea1d822bac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47c128c7b6fd567e9fc1",
+    "url": "/static/css/main.2b6577bf.chunk.css"
+  },
+  {
+    "revision": "2a8cf3aae588161be2db",
+    "url": "/static/js/2.c7e3fb11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c7e3fb11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c128c7b6fd567e9fc1",
+    "url": "/static/js/main.6f62aeff.chunk.js"
+  },
+  {
+    "revision": "efed8f3668ac34eff281",
+    "url": "/static/js/runtime-main.c6c8d8b8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/outstandingboy.com/precache-manifest.58b27ad899c803d8882336414c324b69.js"
+  "/precache-manifest.858a1d24bfecef35591d61fd8529cae7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/outstandingboy.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
